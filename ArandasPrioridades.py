e3b956adffc4196e359a457a44af3e0ac2a38edc
beticarm <beretanamolina@gmail.com>
2026-04-15 10:19:24 -0600

    Enhance form validation for required fields
    Updated validation to check for required fields in the form.

--- ArandasPrioridades.py
+++ ArandasPrioridades.py
@@ -37,7 +37,9 @@ def formulario():
         datos = request.form
         
         # 2. VALIDACIÓN: Verificar que ningún campo esté vacío
-        for campo, valor in datos.items():
+        campos_esperados = ["analista", "sistema", "numero_caso", "prioridad", "observaciones", "defino_3009", "atendido_fecha"]
+        for campo, in campos_esperados:
+            valor = request.form.get(campo) # .get() devuelve None si no existe, no explota
             if not valor or not str(valor).strip():
                 return f"Error: El campo '{campo}' es obligatorio.", 400
 
